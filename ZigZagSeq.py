481f741c4a202e8ab85895a6bc1772c3c64cd62d
solomonshihundu <shihundusolomon@gmail.com>
2023-10-17 03:31:06 +0300

    zigzag list

--- ZigZagSeq.py
+++ ZigZagSeq.py
@@ -3,19 +3,23 @@
 # lexicographically : 1,2,3 = 123,132,213,231,312,321 permutations
 # sample [2,3,5,1,4] >> [1,4,5,3,2]
 def findZigZagSequence(a, n):
+    print("Original : ",a)
     # sort the input array in ASC order
     # [1,2,3,4,5]
     a.sort()
-
-    mid = int((n + 1)/2)            # k variable 
+    print("Sorted : ",a)
+    mid = int((n - 1)/2)            # k variable 
     a[mid], a[n-1] = a[n-1], a[mid] # [1,2,5,4,3]
 
+    print("Swaped  0 : ",a)
+
     st = mid + 1                    # 4
-    ed = n - 1                      # 4
+    ed = n - 2                     # 4
     while(st <= ed):
         a[st], a[ed] = a[ed], a[st] # [1,2,5,3,4] , 
         st = st + 1
-        ed = ed + 1
+        ed = ed - 1
+        print(a)
 
     for i in range (n):
         if i == n-1:
@@ -24,6 +28,8 @@ def findZigZagSequence(a, n):
             print(a[i], end = ' ')
     return
 
-arr = [2,3,5,1,4]
+#arr = [2,3,5,1,4]
+# arr = [1 ,2, 3 ,4, 5,6 ,7]
+arr = [1 ,2, 3 ,4, 5,6 ,7,8,9]
 n = len(arr)
 findZigZagSequence(arr,n)

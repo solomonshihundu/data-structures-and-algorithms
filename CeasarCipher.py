307a15574969ddf20014791e2ccdd33bae73eae8
solomonshihundu <shihundusolomon@gmail.com>
2023-10-19 00:31:21 +0300

    cipher works okay

--- CeasarCipher.py
+++ CeasarCipher.py
@@ -1,39 +1,45 @@
 import string
 
-def cipher(s,n):
+def cipher(s,k):
     
+    # stores our final string
     output = []
 
+    # get separate lists for upper and lower case
     alpha_upper = list(string.ascii_uppercase)
-
     alpha_lower = list(string.ascii_lowercase)
+    
+    alpha_len = len(alpha_upper)
 
+    # remove any padding spaces in the string
     s = s.strip()
 
     for x in s:
+        # If current char is an alphabet proceed with cipher
+        # otherwise append the char to the output list
         if x.isalpha():
+            # Upper case cipher
             if x.isupper():
-                for i in range(len(alpha_upper)):
+                for i in range(alpha_len):
                     if x == alpha_upper[i]:
-                        if i+n > len(alpha_upper):
-                            i = n - i
-                            output.append(alpha_upper[i+n])
-                        else:
-                            output.append(alpha_upper[i+n])
+                        index = alpha_upper.index(x)
+                        pos = (index + k) % (alpha_len)
+                        output.append(alpha_upper[pos])
+            # Lower case cipher
             else:
-                for j in range(len(alpha_lower)):
-                    if x == alpha_lower[j]:
-                        if j + n > len(alpha_lower):
-                            j = n - j
-                            output.append(alpha_lower[j+n])
-                        else:
-                            output.append(alpha_lower[j+n])
-        else:
+                for i in range(alpha_len):
+                    if x == alpha_lower[i]:
+                        index = alpha_lower.index(x)
+                        pos = (index + k) % (alpha_len)
+                        output.append(alpha_lower[pos])
+        else:    
             output.append(x)
     
-    result = ' '.join(map(str,output))
+    result = ''.join(map(str,output))
+
+    print(type(result))
     return result
 
-s = "middle-Outxyz" 
+s = "ab-middle-Oout-xyz" 
 n = 2
-print(cipher(s,n)) #okffng-Qwvb
+print(cipher(s,n)) #cd-okffng-Qqwv-zab
---
+++ Modulus.py
@@ -0,0 +1,6 @@
+def get_mod(n,m):
+    print(n%m)
+
+n = 27
+m = 26
+get_mod(n,m)
